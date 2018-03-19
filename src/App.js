11af18a1b121e2e00616b6300a7dc3a6e5c7c04e
Andrew Locke <alocke12992@gmail.com>
2018-03-19 11:10:35 -0600

    Adds git repos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,104 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import
+{
+  Header,
+  Button,
+  Segment,
+  Card,
+  Icon,
+  Grid,
+} from 'semantic-ui-react';
+import styled from 'styled-components';
+import HeaderText from './HeaderText';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
+const AppContainer = styled.div`
+background: linear-gradient(to bottom right, #004e92, #000428);
+background-size: cover;
+// Light blue to deep blue #1cb5e0, #000046 
+// Sky White to Blue #076585, #fff
+// Evening Night #fffde4, #005aa7
+// Frost #004e92, #000428
+  `
+
+const Transparent = styled.div`
+  background: transparent !important; 
+`
+
+const StyledCard = styled( Card ) `
+  height: 200px;
+`
+
+const Truncated = styled.div`
+  width: 250px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis; 
+`
+
+const fontSize = ( size ) =>
+{
+  switch ( size )
+  {
+    case 'large':
+      return '4rem'
+    case 'small':
+      return '1rem'
+    default:
+      return '2rem'
+  }
+}
+
+class App extends React.Component
+{
+  state = { repos: [] }
+
+  componentDidMount()
+  {
+    axios.get( 'https://api.github.com/users/alocke12992/repos?sort=created' )
+      .then( res => this.setState( { repos: res.data } ) )
+  }
+
+  render()
+  {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <AppContainer>
+        <Header fSize='large' as={ HeaderText }>
+          My Portfolio
+        </Header>
+        <Segment as={ Transparent }>
+          <Header as={ HeaderText }>
+            My Projects
+          </Header>
+          <Grid>
+            <Grid.Row>
+              { this.state.repos.map( r =>
+                <Grid.Column key={ r.id } width={ 4 }>
+                  <StyledCard>
+                    <Card.Content>
+                      <Card.Header>
+                        <Truncated>
+                          { r.full_name }
+                        </Truncated>
+                      </Card.Header>
+                      <Card.Meta>
+                        { r.description }
+                      </Card.Meta>
+                    </Card.Content>
+                  </StyledCard>
+                </Grid.Column>
+              )
+              }
+            </Grid.Row>
+          </Grid>
+        </Segment>
+        <Segment as={ Transparent }>
+          <Header as={ HeaderText }>
+            Contact
+          </Header>
+        </Segment>
+      </AppContainer>
+    )
   }
 }
 
